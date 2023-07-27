12a5f4de9dba961307eb159b5761083cc802fc8b
bernard <bernardaryee4@gmail.com>
2023-07-27 06:20:26 +0000

    Resolve Prometheus Deps

--- api-gateway/src/main/java/com/rancard/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/rancard/apigateway/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.rancard.apigateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebFluxSecurity
+//@Configuration
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
 //    @Bean
