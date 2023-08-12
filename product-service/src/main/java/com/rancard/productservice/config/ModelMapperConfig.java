324de6b8b75ad13721e113d3a51c8bc75c644a14
bernard <bernardaryee4@gmail.com>
2023-08-12 03:15:28 +0000

    Added model mapper bean

---
+++ product-service/src/main/java/com/rancard/productservice/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.rancard.productservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+}
