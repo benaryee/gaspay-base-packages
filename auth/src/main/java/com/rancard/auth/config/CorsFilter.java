c7da22db8e6ac40a204a77f6e91bfede3aeadd13
bernard <bernardaryee4@gmail.com>
2023-09-07 09:37:45 +0000

    [Troubleshoot]: Create Separate Mongo DBs for each Microservice

---
+++ auth/src/main/java/com/rancard/auth/config/CorsFilter.java
@@ -0,0 +1,41 @@
+package com.rancard.auth.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, PATCH");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Authorization, Content-Type, X-Tenant-Id, X-Client, X-Lang");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "3600");
+
+        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig config) {
+    }
+}
