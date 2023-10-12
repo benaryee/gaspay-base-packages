7b5ea873df81a117d924fcf85bd63eea2fa794a1
bernard <bernardaryee4@gmail.com>
2023-10-12 02:12:16 +0000

    [Update]: Updated properties to add mongo product auth

---
+++ auth/src/test/java/com/rancard/auth/controller/AuthControllerTest.java
@@ -0,0 +1,70 @@
+package com.rancard.auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rancard.auth.model.dto.SignupDto;
+import com.rancard.auth.service.AuthService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(controllers = AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+
+    @MockBean
+    private MongoTemplate mongoTemplate;
+
+    @MockBean
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private SignupDto signupDto;
+
+    @BeforeEach
+    public void init(){
+        signupDto = SignupDto.builder()
+                .firstName("Bernard")
+                .lastName("Aryee")
+                .email("bernardaryee4@gmail.com")
+                .phone("0541877221")
+                .build();
+    }
+
+    @Test
+    public void AuthController_CreateUser_ReturnCreated() throws Exception{
+        given(authService.signUpUser(ArgumentMatchers.any())).willAnswer(invocation -> invocation.getArgument(0));
+
+        ResultActions response = mockMvc.perform(post("/api/auth/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signupDto))
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated());
+
+    }
+}
