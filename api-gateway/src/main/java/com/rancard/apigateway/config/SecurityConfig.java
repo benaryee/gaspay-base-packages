a6a71fd6354346199016a303c456132345487297
bernard <bernardaryee4@gmail.com>
2023-10-26 08:24:35 +0000

    [Update]: Added base models module

--- api-gateway/src/main/java/com/rancard/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/rancard/apigateway/config/SecurityConfig.java
@@ -1,25 +1,22 @@
 package com.rancard.apigateway.config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity
-                .csrf().disable()
-                .authorizeExchange(exchange ->
-                        exchange.pathMatchers("/eureka/**", "/api/payment/**" ,
-                                        "/api/ussd", "/api/product/**", "/api/payment/topup/callback", "/api/auth/**")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
-    }
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity
+//                .csrf().disable()
+//                .authorizeExchange(exchange ->
+//                        exchange.pathMatchers("/api/ussd/menu","/eureka/**", "/api/payment/**" ,
+//                                        "/api/ussd", "/api/product/**", "/api/payment/topup/callback", "/api/auth/**")
+//                                .permitAll()
+//                                .anyExchange()
+//                                .authenticated())
+//                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+//        return serverHttpSecurity.build();
+//    }
 }
