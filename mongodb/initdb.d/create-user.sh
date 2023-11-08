5ecd464256641f7a10b13b50734e239678a60623
bernard <bernardaryee4@gmail.com>
2023-11-08 04:42:11 +0000

    Docker update

--- mongodb/initdb.d/create-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mongo -u root -p root --authenticationDatabase admin chap --eval "db.createUser({ user: 'product', pwd: 'password', roles: [{ role: 'dbOwner', db: 'product-service' }] })"
-mongo -u root -p root --authenticationDatabase admin chap  --eval "db.createUser({ user: 'ussd', pwd: 'password', roles: [{ role: 'dbOwner', db: 'ussd' }] })"
-mongo -u root -p root --authenticationDatabase admin chap --eval "db.createUser({ user: 'order', pwd: 'password', roles: [{ role: 'dbOwner', db: 'order-service' }] })"
-mongo -u root -p root --authenticationDatabase admin chap --eval "db.createUser({ user: 'auth', pwd: 'password', roles: [{ role: 'dbOwner', db: 'auth' }] })"
-mongo -u root -p root --authenticationDatabase admin chap --eval "db.createUser({ user: 'payment', pwd: 'password', roles: [{ role: 'dbOwner', db: 'payment-service' }] })"
